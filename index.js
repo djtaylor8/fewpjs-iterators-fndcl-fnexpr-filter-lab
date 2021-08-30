d30e26c8ce598d097ab9f4fc296c9e5256b52852
DJ Taylor <dtaylor88@gmail.com>
2021-08-29 22:24:12 -0700

    commit tree 5e068aecd583dc927b62382e38904ca95030df17

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+
+
+
+function findMatching(array, str) {
+  return array.filter(function(el) {
+     return el.toLowerCase().indexOf(str.toLowerCase()) !== -1
+  });
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter(function(el) {
+        return el.toLowerCase().charAt(0) === str.toLowerCase().charAt(0)
+     });
+}
+
+function matchName(array, str) {
+    return array.filter(function(el) {
+        return el.name.toLowerCase().indexOf(str.toLowerCase()) !== -1
+    })
+}
